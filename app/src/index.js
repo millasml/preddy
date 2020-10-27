e93398a30bf5026ebd95faae0e72ae01d5a9dcb6
millasml <millasml@yahoo.com.sg>
2020-10-28 01:21:27 +0800

    frontend: moved contract stuff to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { DrizzleContext } from "@drizzle/react-plugin";
+import { Drizzle } from "@drizzle/store";
+import drizzleOptions from "./drizzleOptions";
+import * as serviceWorker from "./serviceWorker";
+import Sample from "./Sample";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const drizzle = new Drizzle(drizzleOptions);
+
+function App() {
+  return (
+    <DrizzleContext.Provider drizzle={drizzle}>
+      <DrizzleContext.Consumer>
+        {(drizzleContext) => {
+          const { drizzle, drizzleState, initialized } = drizzleContext;
+
+          if (!initialized) {
+            return "Loading...";
+          }
+
+          return <Sample drizzle={drizzle} drizzleState={drizzleState} />;
+        }}
+      </DrizzleContext.Consumer>
+    </DrizzleContext.Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
