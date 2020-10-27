68dcdc28f4f380f8d69a32be1bc38fb223a3f2f0
millasml <millasml@yahoo.com.sg>
2020-10-28 01:28:31 +0800

    frontend: added theme and main page

--- app/src/index.js
+++ app/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
 import drizzleOptions from "./drizzleOptions";
-import * as serviceWorker from "./serviceWorker";
-import Sample from "./Sample";
+
+import Main from "./pages/main";
 
 const drizzle = new Drizzle(drizzleOptions);
 
@@ -19,7 +19,7 @@ function App() {
             return "Loading...";
           }
 
-          return <Sample drizzle={drizzle} drizzleState={drizzleState} />;
+          return <Main drizzle={drizzle} drizzleState={drizzleState} />;
         }}
       </DrizzleContext.Consumer>
     </DrizzleContext.Provider>
@@ -27,8 +27,3 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
